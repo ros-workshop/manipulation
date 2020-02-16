24c8535b3092579d2eb46ad1e5e05da9d60a3c69
thibautrouillard <thibault.rouillard@postgrad.curtin.edu.au>
2020-02-16 19:17:51 +0800

    midified the grasp server

--- Workshop/manipulation/src/object_grasp_server.cpp
+++ Workshop/manipulation/src/object_grasp_server.cpp
@@ -12,23 +12,36 @@
 #include <gazebo_ros_link_attacher/Attach.h>
 // start of the GraspTag class//
 
+
+
+/*************ADD SOME CODE HERE (START)**********
+|
+|
+|
+|
+|
+|
+************ADD SOME CODE HERE (END)**************/
+
+// The above shows you where code should be added
+
+// Look for spelling mistake and commented lines
+
+// Only read from line 178
+
 using namespace tf;
 
 void SigintHandler(int sig)
 {
-  // Do some custom action.
-  // For example, publish a stop message to some other nodes.
-  
-  // All the default sigint handler does is call shutdown()
-  ROS_INFO("SHUTING DOWN()");
-  ros::shutdown();
-}
 
+	ROS_INFO("SHUTING DOWN()");
+	ros::shutdown();
+}
 
 class GraspTag
 {
 
-  public:
+public:
 	GraspTag();
 
 	void setPoseCallback(const geometry_msgs::Pose::ConstPtr &pose);
@@ -40,11 +53,12 @@ class GraspTag
 	void AttachGripper();
 	void DetachGripper();
 	geometry_msgs::Pose getTarget();
-	
-  private:
+
+private:
 	geometry_msgs::Pose target_pose;
 	ros::NodeHandle n;
-	ros::Subscriber pose_sub;;
+	ros::Subscriber pose_sub;
+	;
 	bool _execute_grasp;
 	bool _succeeded;
 	bool _srv_success;
@@ -54,16 +68,14 @@ class GraspTag
 	gazebo_ros_link_attacher::Attach _attach_srv;
 	ros::ServiceClient attach_tag_client;
 	ros::ServiceClient detach_tag_client;
-	
 };
-
 
 GraspTag::GraspTag()
 {
 
 	signal(SIGINT, SigintHandler);
 	pose_sub = n.subscribe("/tag_pose", 1000, &GraspTag::setPoseCallback, this);
-	gripper_grasp_pub = n.advertise<trajectory_msgs::JointTrajectory>("/gripper_controller/command",1000);
+	gripper_grasp_pub = n.advertise<trajectory_msgs::JointTrajectory>("/gripper_controller/command", 1000);
 	attach_tag_client = n.serviceClient<gazebo_ros_link_attacher::Attach>("/link_attacher_node/attach");
 	detach_tag_client = n.serviceClient<gazebo_ros_link_attacher::Attach>("/link_attacher_node/detach");
 	DontExecuteGrasp();
@@ -71,36 +83,37 @@ GraspTag::GraspTag()
 	_attach_srv.request.link_name_1 = "wrist_3_link";
 	_attach_srv.request.model_name_2 = "apriltag";
 	_attach_srv.request.link_name_2 = "my_box";
-
 }
 
+bool GraspTag::serviceCallback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res)
+{
 
-bool GraspTag::serviceCallback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res){
+	ROS_INFO("service called");
 
-		ROS_INFO("service called");
-		
-		_execute_grasp = true;
-		_succeeded = false;
-		while(_succeeded==false && !ros::isShuttingDown())
-		{sleep(0.1);}
-		if(_srv_success){
+	_execute_grasp = true;
+	_succeeded = false;
+	while (_succeeded == false && !ros::isShuttingDown())
+	{
+		sleep(0.1);
+	}
+	if (_srv_success)
+	{
 		res.success = _srv_success;
 		res.message = "all motion succeeded";
-		}
-		else{
-		res.success = _srv_success;	
+	}
+	else
+	{
+		res.success = _srv_success;
 		res.message = "falied to exceute all motion";
-		}
+	}
 
-		return true;
-
+	return true;
 }
 
-void GraspTag::setPoseCallback(const geometry_msgs::Pose::ConstPtr& pose)
+void GraspTag::setPoseCallback(const geometry_msgs::Pose::ConstPtr &pose)
 {
 	target_pose.position = pose->position;
-	target_pose.orientation = createQuaternionMsgFromRollPitchYaw(1.57,0, 0);
-
+	target_pose.orientation = createQuaternionMsgFromRollPitchYaw(1.57, 0, 0);
 }
 
 geometry_msgs::Pose GraspTag::getTarget()
@@ -115,8 +128,8 @@ void GraspTag::DontExecuteGrasp()
 
 void GraspTag::setSuccess(bool success)
 {
-	_srv_success= success;
-	_succeeded=true;
+	_srv_success = success;
+	_succeeded = true;
 }
 
 bool GraspTag::ExecuteGrasp()
@@ -124,12 +137,13 @@ bool GraspTag::ExecuteGrasp()
 	return _execute_grasp;
 }
 
-void GraspTag::CloseGripper(float angle){
+void GraspTag::CloseGripper(float angle)
+{
 	ros::Rate _loop_rate(10);
-	if (angle<0.0)
-	angle = 0.0;
-	else if (angle>1.5707)
-	angle = 1.5707;
+	if (angle < 0.0)
+		angle = 0.0;
+	else if (angle > 1.5707)
+		angle = 1.5707;
 
 	_gripper_angle_traj.joint_names.push_back("simple_gripper_right_driver_joint");
 	_gripper_angle_point.positions.push_back(angle);
@@ -137,9 +151,10 @@ void GraspTag::CloseGripper(float angle){
 	_gripper_angle_traj.points.push_back(_gripper_angle_point);
 
 	ros::Time now = ros::Time::now();
-	ros::Time  then = ros::Time::now();
+	ros::Time then = ros::Time::now();
 
-	while((then-now).toSec()<1.0){
+	while ((then - now).toSec() < 1.0)
+	{
 		gripper_grasp_pub.publish(_gripper_angle_traj);
 		then = ros::Time::now();
 		_loop_rate.sleep();
@@ -148,15 +163,15 @@ void GraspTag::CloseGripper(float angle){
 	_gripper_angle_point.positions.clear();
 	_gripper_angle_traj.points.clear();
 	_gripper_angle_traj.joint_names.clear();
+}
 
-	}
-
-
-void GraspTag::AttachGripper(){
+void GraspTag::AttachGripper()
+{
 	attach_tag_client.call(_attach_srv);
 }
 
-void GraspTag::DetachGripper(){
+void GraspTag::DetachGripper()
+{
 	detach_tag_client.call(_attach_srv);
 }
 
@@ -175,8 +190,8 @@ int main(int argc, char **argv)
 	geometry_msgs::Pose temp_pose; //temporary pose to check when the same target is receive
 	GraspTag grasObj;			   // instance of the class GraspTag
 
-	moveit::planning_interface::MoveGroupInterface::Plan my_plan; // plan containing the trajectory
-	static const std::string PLANNING_GROUP = "manipulator";		  // planning group
+	moveit::planning_interface::MoveGroupInterface::Plan my_plan;				 // plan containing the trajectory
+	static const std::string PLANNING_GROUP = "manipulator";					 // planning group
 	moveit::planning_interface::PlanningSceneInterface planning_scene_interface; // planning interface
 	moveit::planning_interface::MoveGroupInterface arm(PLANNING_GROUP);			 // planning group
 	arm.setPlannerId("RRTConnect");
@@ -185,13 +200,11 @@ int main(int argc, char **argv)
 	arm.setPlanningTime(5.0);
 	// end of declarations
 
-	
-
 	ROS_INFO("Reference frame: %s", arm.getPlanningFrame().c_str());
 	ROS_INFO("Reference frame: %s", arm.getEndEffectorLink().c_str());
 
 	temp_pose = (arm.getCurrentPose(arm.getEndEffectorLink().c_str())).pose;
-	
+
 	ROS_INFO("Pose:HOME");
 	arm.setNamedTarget("home");
 	arm.plan(my_plan); // check if plan succeded
@@ -199,12 +212,8 @@ int main(int argc, char **argv)
 
 	sleep(4.0);
 
-
-
 	ros::ServiceServer service = n.advertiseService("grasp", &GraspTag::serviceCallback, &grasObj); // creating the server called grasp
 
-	
-	 
 	while (ros::ok) // keep on running until stoped
 	{
 		grasObj.DontExecuteGrasp();
@@ -213,26 +222,25 @@ int main(int argc, char **argv)
 		ROS_INFO("tag detected");
 
 		ROS_INFO("Ready for Grasp service call");
-		while(!grasObj.ExecuteGrasp() && !ros::isShuttingDown()){
+		while (!grasObj.ExecuteGrasp() && !ros::isShuttingDown())
+		{
 			//wait
 			loop_rate.sleep();
 		}
 		ROS_INFO("Grasp server active");
 		temp_pose = grasObj.getTarget(); //set the desired pose to the position of the cube
-		
-		ROS_INFO("Tag location:\n x:%lf \n c:%lf \n z:%lf",temp_pose.position.x,temp_pose.position.y,temp_pose.position.z);
-		
-		temp_pose.position.z += 0.25;	  //offset by .25 meters in the z axis
+
+		ROS_INFO("Tag location:\n x:%lf \n c:%lf \n z:%lf", temp_pose.position.x, temp_pose.position.y, temp_pose.position.z);
+
+		temp_pose.position.z += 0.25; //offset by .25 meters in the z axis
 		temp_pose.position.x -= 0.05;
 		temp_pose.position.y += 0.05;
-		
 
-		temp_pose.orientation.w= 0.707;
-		temp_pose.orientation.x= 0.0;
-		temp_pose.orientation.y= 0.707;
-		temp_pose.orientation.z= 0.0;   // and pointed straight down
+		temp_pose.orientation.w = 0.707;
+		temp_pose.orientation.x = 0.0;
+		temp_pose.orientation.y = 0.707;
+		temp_pose.orientation.z = 0.0; // and pointed straight down
 
-	
 		arm.setPoseTarget(temp_pose, arm.getEndEffectorLink().c_str());
 
 		arm.plan(my_plan); // check if plan succeded
@@ -242,59 +250,67 @@ int main(int argc, char **argv)
 		//sleep(5.0);
 		sleep(1.0);
 
-		temp_pose.position.z -= 0.08; // move down towards the cube
-		arm.setPoseTarget(temp_pose, arm.getEndEffectorLink().c_str());
+		/*************ADD SOME CODE HERE (START)**********
+		|
+		|
+		|
+		|
+		|
+		|
+		************ADD SOME CODE HERE (END)**************/
 
-		arm.plan(my_plan); // check if plan succeded
-
-		arm.move();
 		sleep(1.0);
 		ROS_INFO("Closing Gripper");
-		
-		grasObj.CloseGripper(0.25); // closing gripper
-	
 
+		grasObj.CloseGripper(0.25); // closing gripper
 
 		//sleep(2.0);
 		sleep(1.0);
-		grasObj.AttachGripper(); //attaching object
+		//grasObj.AttachGripper(); //attaching object
 		ROS_INFO("Attaching objects");
-		
-		temp_pose.position.z += 0.2;//raising cube in the air
+
+		temp_pose.position.z += 0.2; //raising cube in the air
+
 		arm.setPoseTarget(temp_pose, arm.getEndEffectorLink().c_str());
 		arm.plan(my_plan); // check if plan succeded
 		arm.move();
 
 		// moving object to next stand
 
-		temp_pose.position.x;
-		temp_pose.position.y-=0.7;
-		temp_pose.position.z-=0.2;
-		ROS_INFO("Moving to new position");
+		/*************ADD SOME CODE HERE (START)**********
+		|
+		|
+		|
+		|
+		|
+		|
+		************ADD SOME CODE HERE (END)**************/
+
 		sleep(1.0);
 
 		arm.setPoseTarget(temp_pose, arm.getEndEffectorLink().c_str());
 		arm.plan(my_plan); // check if plan succeded
 		arm.move();
 		sleep(1.0);
+
 		ROS_INFO("Detaching Object");
 		grasObj.DetachGripper(); // Detaching object
 		grasObj.CloseGripper(0.0);
 		sleep(1);
+
+
 		ROS_INFO("Moving to Home");
-		arm.setNamedTarget("home");// This is needed so that the robot arm will not block the LIDAR
+		arm.setNamedTarget("home"); // This is needed so that the robot arm will not block the LIDAR
 		arm.plan(my_plan);
-		arm.move();				
+		arm.move();
 
 		ROS_INFO("finished motion plan");
 
-		
 		grasObj.setSuccess(true);
-		
 
 		ros::spinOnce();
 
 		loop_rate.sleep();
 	}
 	return 0;
-}	
+}
