d63268bb960935aacc1955714587c931755eba2f
Andrew Sherry <andrew.sherry@chironix.com>
2022-02-04 11:25:17 +0800

    moveit example signposting

--- Workshop/manipulation/src/moveit_expl.cpp
+++ Workshop/manipulation/src/moveit_expl.cpp
@@ -6,13 +6,11 @@
 int main(int argc, char **argv)
 {
 	// Declarations
-
 	ros::init(argc, argv, "moveit_expl");
 	ros::NodeHandle node_handle;
 	ros::AsyncSpinner spinner(1);
 	spinner.start();
-	ros::Rate loop_rate(1);
-	geometry_msgs::Pose temp_pose; // Working pose for calcs and sub goals
+	geometry_msgs::Pose working_pose; // Working pose for calcs and sub goals
 
 	moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 	static const std::string PLANNING_GROUP = "manipulator";
@@ -21,7 +19,7 @@ int main(int argc, char **argv)
 
 	// Can be modified as desired
 	arm.setPlannerId("RRTConnectkConfigDefault");
-	arm.setGoalTolerance(0.001);
+	arm.setGoalTolerance(0.01);
 	arm.setPlanningTime(20.0);
 
 	// First we add a fake object to prevent the robot from hitting the floor
@@ -30,11 +28,10 @@ int main(int argc, char **argv)
 	attached_object.id = "floor";
 
 	// Floor centred below robot default pose
-	geometry_msgs::Pose pose;
-	pose.orientation.w = 1.0;
-	pose.position.x = 0.76;
-	pose.position.y = 0.2;
-	pose.position.z = -0.30;
+	working_pose.orientation.w = 1.0;
+	working_pose.position.x = 0.76;
+	working_pose.position.y = 0.2;
+	working_pose.position.z = -0.30;
 
 	// Floor is a flat box
 	shape_msgs::SolidPrimitive primitive;
@@ -46,7 +43,7 @@ int main(int argc, char **argv)
 
 	// Add the floor
 	attached_object.primitives.push_back(primitive);
-	attached_object.primitive_poses.push_back(pose);
+	attached_object.primitive_poses.push_back(working_pose);
 	attached_object.operation = attached_object.ADD;
 	ROS_INFO("adding floor");
 	planning_scene_interface.applyCollisionObject(attached_object);
@@ -57,33 +54,38 @@ int main(int argc, char **argv)
 	ROS_INFO("Reference frame: %s", arm.getEndEffectorLink().c_str());
 
 	// Showing off Pose Goal
-	temp_pose = (arm.getCurrentPose(arm.getEndEffectorLink().c_str())).pose;
-	ROS_WARN("Moving the arm");
-	temp_pose.position.x -= 0.1;
-	arm.setPoseTarget(temp_pose);
+	ROS_WARN
+	(
+		"Moving arm to Pose:\nx:%f\ny:%f\nz:%f\n",
+		working_pose.position.x,
+		working_pose.position.y,
+		working_pose.position.z
+	);
+	working_pose = (arm.getCurrentPose(arm.getEndEffectorLink().c_str())).pose;
+	working_pose.position.x -= 0.1;
+	arm.setPoseTarget(working_pose);
 	arm.plan(my_plan);
 	arm.move();
 
 	// Showing off Named Goal
-	ROS_WARN("Moving the arm");
+	ROS_WARN("Moving arm to Named Goal: \"home\"");
 	arm.setNamedTarget("home");
 	arm.plan(my_plan);
 	arm.move();
 
 	// Showing off Joint Goal
-	ROS_WARN("Moving the arm");
+	ROS_WARN("Moving arm to Joint Goal: \"shoulder_pan_joint\": -1.57");
 	arm.setJointValueTarget("shoulder_pan_joint", -1.57);
 	arm.plan(my_plan);
 	arm.move();
 
 	// Returning arm Home
-	ROS_WARN("Moving the arm");
+	ROS_WARN("Moving arm back to home");
 	arm.setNamedTarget("home");
 	arm.plan(my_plan);
 	arm.move();
 
 	// Fin,
 	ros::shutdown();
-	loop_rate.sleep();
 	return 0;
 }
