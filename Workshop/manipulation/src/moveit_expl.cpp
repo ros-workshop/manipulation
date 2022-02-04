6ab48eebf01259a9e9824810be93b3d7b632cb56
Andrew Sherry <andrew.sherry@chironix.com>
2022-02-04 09:29:29 +0800

    Formatting, structur, and typos

--- Workshop/manipulation/src/moveit_expl.cpp
+++ Workshop/manipulation/src/moveit_expl.cpp
@@ -5,44 +5,38 @@
 
 int main(int argc, char **argv)
 {
-	//delcarations
+	// Declarations
 
 	ros::init(argc, argv, "moveit_expl");
 	ros::NodeHandle node_handle;
 	ros::AsyncSpinner spinner(1);
 	spinner.start();
 	ros::Rate loop_rate(1);
-	geometry_msgs::Pose temp_pose; //temporary pose to check when the same target is receive
+	geometry_msgs::Pose temp_pose; // Working pose for calcs and sub goals
 
-	moveit::planning_interface::MoveGroupInterface::Plan my_plan;				 // plan containing the trajectory
-	static const std::string PLANNING_GROUP = "manipulator";					 // planning group
-	moveit::planning_interface::PlanningSceneInterface planning_scene_interface; // planning interface
-	moveit::planning_interface::MoveGroupInterface arm(PLANNING_GROUP);			 // planning group
+	moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+	static const std::string PLANNING_GROUP = "manipulator";
+	moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+	moveit::planning_interface::MoveGroupInterface arm(PLANNING_GROUP);
+
+	// Can be modified as desired
 	arm.setPlannerId("RRTConnectkConfigDefault");
-	//can be modified as desired
 	arm.setGoalTolerance(0.001);
 	arm.setPlanningTime(20.0);
-	//arm.setPlanningTime(10.0);
-	//arm.setPlanningTime(15.0);
 
-	// end of declarations
-
-	/*First we add a fake object to prevent the robot from hitting the floor */
-
+	// First we add a fake object to prevent the robot from hitting the floor
 	moveit_msgs::CollisionObject attached_object;
-	/* The header must contain a valid TF frame*/
 	attached_object.header.frame_id = "world";
-	/* The id of the object */
 	attached_object.id = "floor";
 
-	/* A default pose */
+	// Floor centred below robot default pose
 	geometry_msgs::Pose pose;
 	pose.orientation.w = 1.0;
 	pose.position.x = 0.76;
 	pose.position.y = 0.2;
 	pose.position.z = -0.30;
-	
-	/* Define a box to be attached */
+
+	// Floor is a flat box
 	shape_msgs::SolidPrimitive primitive;
 	primitive.type = primitive.BOX;
 	primitive.dimensions.resize(3);
@@ -50,52 +44,46 @@ int main(int argc, char **argv)
 	primitive.dimensions[1] = 5.0;
 	primitive.dimensions[2] = 0.05;
 
+	// Add the floor
 	attached_object.primitives.push_back(primitive);
 	attached_object.primitive_poses.push_back(pose);
 	attached_object.operation = attached_object.ADD;
-
-	//**********************Defining first collision object End**********************//
-
 	ROS_INFO("adding floor");
-	//moveit_msgs::CollisionObject remove_object;
-
-	
 	planning_scene_interface.applyCollisionObject(attached_object);
 	sleep(1.0);
 
+	// Information for workshop action
 	ROS_INFO("Reference frame: %s", arm.getPlanningFrame().c_str());
 	ROS_INFO("Reference frame: %s", arm.getEndEffectorLink().c_str());
 
+	// Showing off Pose Goal
 	temp_pose = (arm.getCurrentPose(arm.getEndEffectorLink().c_str())).pose;
-
-	/*pose goal */
 	ROS_WARN("Moving the arm");
 	temp_pose.position.x -= 0.1;
 	arm.setPoseTarget(temp_pose);
 	arm.plan(my_plan);
 	arm.move();
 
-	/*name goal */
+	// Showing off Named Goal
 	ROS_WARN("Moving the arm");
 	arm.setNamedTarget("home");
 	arm.plan(my_plan);
 	arm.move();
 
-	/*joint goal */
+	// Showing off Joint Goal
 	ROS_WARN("Moving the arm");
 	arm.setJointValueTarget("shoulder_pan_joint", -1.57);
 	arm.plan(my_plan);
 	arm.move();
 
-	/*name goal */
+	// Returning arm Home
 	ROS_WARN("Moving the arm");
 	arm.setNamedTarget("home");
 	arm.plan(my_plan);
 	arm.move();
 
+	// Fin,
 	ros::shutdown();
-
 	loop_rate.sleep();
-
 	return 0;
 }
